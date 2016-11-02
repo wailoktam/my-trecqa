4648daa8d8a50090a42c5ffb6186a7ff07135854
namgi <tinynight@me.com>
2016-11-02 17:47:11 +0900

    convert tab to 4 space

--- processTrecQA.py
+++ processTrecQA.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding: -*- utf-8 -*-
+# coding: utf-8
 
 import codecs
 from pycorenlp import StanfordCoreNLP
@@ -15,92 +15,92 @@ num_p = re.compile('<[0-9]*>')
 lemmaSet = ()
 
 def lparse(input):
-	output = nlp.annotate(input, properties={
-		'annotators':'tokenize,ssplit,pos.lemma',
-		'outputFormat':'xml',
-		'timeout':30000
-		})
-	fixed = []
-	for o in output:
-		if is_ascii(o):
-			fixed.append(o)
-	return("".join(fixed))
+    output = nlp.annotate(input, properties={
+        'annotators':'tokenize,ssplit,pos.lemma',
+        'outputFormat':'xml',
+        'timeout':30000
+        })
+    fixed = []
+    for o in output:
+        if is_ascii(o):
+            fixed.append(o)
+    return("".join(fixed))
 
 def convert_term_to_num(input):
 
 
 def process(input):
-	num = ''
-	count = 0
+    num = ''
+    count = 0
 
-	data = {}
+    data = {}
 
-	for line in codecs.open(input, 'r', 'utf-8'):
-		lemma_sent = []
+    for line in codecs.open(input, 'r', 'utf-8'):
+        lemma_sent = []
 
-		if num_p.match(line) and '¥t' not in line:
-			num = int(line.replace('<','').replace('>','').strip())
-			count = 1
-			if not lemma_sent and not num:
-				data[num] = lemma_sent
+        if num_p.match(line) and '¥t' not in line:
+            num = int(line.replace('<','').replace('>','').strip())
+            count = 1
+            if not lemma_sent and not num:
+                data[num] = lemma_sent
 
-		if count == 0:
-			sent = " ".join(line.split('¥t'))
-			lparseXML = etree.fromstring(lparse(sent))
+        if count == 0:
+            sent = " ".join(line.split('¥t'))
+            lparseXML = etree.fromstring(lparse(sent))
 
-			tokens = lparseXML.findall('.//token')
-				
-			for token in tokens:
-				lemma = token.find(".//lemma").text
-				lemma_sent.append(lemma)
-				lemmaSet.add(lemma)
-		
-		count -= 1
+            tokens = lparseXML.findall('.//token')
+                
+            for token in tokens:
+                lemma = token.find(".//lemma").text
+                lemma_sent.append(lemma)
+                lemmaSet.add(lemma)
+        
+        count -= 1
 
-	return data
+    return data
 
 if __name__ == '__main__':
-	train_q = process(data_path + 'train.question')
-	test_q = process(data_path + 'test.question')
-	dev_q = process(data_path + 'dev.question')
+    train_q = process(data_path + 'train.question')
+    test_q = process(data_path + 'test.question')
+    dev_q = process(data_path + 'dev.question')
 
-	train_a = process(data_path + 'train.answer')
-	test_a = process(data_path + 'test.answer')
-	dev_a = process(data_path + 'dev.answer')
+    train_a = process(data_path + 'train.answer')
+    test_a = process(data_path + 'test.answer')
+    dev_a = process(data_path + 'dev.answer')
 
-	lem_num = 0
-	lemmaDic = {}
-	revlemmaDic = {}
+    lem_num = 0
+    lemmaDic = {}
+    revlemmaDic = {}
 
-	for term in lemmaSet:
-		lem_num += 1
-		lemmaDic[term] = lem_num
-		revlemmaDic[lem_num] = term
+    for term in lemmaSet:
+        lem_num += 1
+        lemmaDic[term] = lem_num
+        revlemmaDic[lem_num] = term
 
-	print '# of all voca : ' + str(len(lemmaDic.keys))
+    print '# of all voca : ' + str(len(lemmaDic.keys))
 
-	pickle.dump(lemmaDic, open('voca', 'w'))
-	pickle.dump(revlemmaDic, open('revVoca', 'w'))
+    pickle.dump(lemmaDic, open('voca', 'w'))
+    pickle.dump(revlemmaDic, open('revVoca', 'w'))
 
     ans_count = 0
     ans_list = []
     ans_dic = {}
 
     for ans_datas in [train_a, test_a, dev_a]:
-    	for ans_id in ans_datas.keys:
-    		ans_count += 1
-    		sent = ans_datas[ans_id]
-    		new_sent = []
-    		for term in sent:
-    			new_sent.append(lemmaDic[term])
-    		temp_dic['id'] = ans_count
-    		temp_dic['text'] = new_sent
-    		if ans_id not in ans_dic:
-    			temp_list = [ans_count]
-    			ans_dic[ans_id] = temp_list
-    		else:
-    			ans_dic[ans_id].append(ans_count)
-    		ans_list.append(temp_dic)
+        for ans_id in ans_datas.keys:
+            ans_count += 1
+            sent = ans_datas[ans_id]
+            new_sent = []
+            for term in sent:
+                new_sent.append(lemmaDic[term])
+            temp_dic['id'] = ans_count
+            temp_dic['text'] = new_sent
+            if ans_id not in ans_dic:
+                temp_list = [ans_count]
+                ans_dic[ans_id] = temp_list
+            else:
+                ans_dic[ans_id].append(ans_count)
+            ans_list.append(temp_dic)
 
     print '# of all answers : ' + str(len(ans_list))
 
@@ -109,17 +109,17 @@ if __name__ == '__main__':
     train_list = []
 
     for train_id in train_q.keys:
-    	temp['question_id'] = train_id
-    	temp['good'] = ans_dic[train_id][0]
+        temp['question_id'] = train_id
+        temp['good'] = ans_dic[train_id][0]
 
-    	new_sent = []
-    	for term in train_q[train_id]:
-    		new_sent.append(lemmaDic[term])
-    	temp['question'] = new_sent
+        new_sent = []
+        for term in train_q[train_id]:
+            new_sent.append(lemmaDic[term])
+        temp['question'] = new_sent
 
-    	temp['bad'] = random.sample(range(1, len(ans_list)).remove(temp['good']), 20)
+        temp['bad'] = random.sample(range(1, len(ans_list)).remove(temp['good']), 20)
 
-    	train_list.append(temp)
+        train_list.append(temp)
 
     print '# of train questions : ' + str(len(train_list))
     pickle.dump(train_list, open('trains', 'w'))
@@ -127,17 +127,17 @@ if __name__ == '__main__':
     test_list = []
 
     for test_id in test_q.keys:
-    	temp['question_id'] = test_id
-    	temp['good'] = ans_dic[test_id][0]
+        temp['question_id'] = test_id
+        temp['good'] = ans_dic[test_id][1]
 
-    	new_sent = []
-    	for term in test_q[test_id]:
-    		new_sent.append(lemmaDic[term])
-    	temp['question'] = new_sent
+        new_sent = []
+        for term in test_q[test_id]:
+            new_sent.append(lemmaDic[term])
+        temp['question'] = new_sent
 
-    	temp['bad'] = random.sample(range(1, len(ans_list)).remove(temp['good']), 20)
+        temp['bad'] = random.sample(range(1, len(ans_list)).remove(temp['good']), 20)
 
-    	test_list.append(temp)
+        test_list.append(temp)
 
     print '# of test questions : ' + str(len(test_list))
     pickle.dump(test_list, open('tests', 'w'))
@@ -145,17 +145,17 @@ if __name__ == '__main__':
     dev_list = []
 
     for dev_id in dev_q.keys:
-    	temp['question_id'] = dev_id
-    	temp['good'] = ans_dic[dev_id][0]
+        temp['question_id'] = dev_id
+        temp['good'] = ans_dic[dev_id][2]
 
-    	new_sent = []
-    	for term in dev_q[dev_id]:
-    		new_sent.append(lemmaDic[term])
-    	temp['question'] = new_sent
+        new_sent = []
+        for term in dev_q[dev_id]:
+            new_sent.append(lemmaDic[term])
+        temp['question'] = new_sent
 
-    	temp['bad'] = random.sample(range(1, len(ans_list)).remove(temp['good']), 20)
+        temp['bad'] = random.sample(range(1, len(ans_list)).remove(temp['good']), 20)
 
-    	dev_list.append(temp)
+        dev_list.append(temp)
 
     print '# of dev questions : ' + str(len(trin_list))
     pickle.dump(dev_list, open('devs', 'w'))
@@ -170,7 +170,7 @@ if __name__ == '__main__':
 
 
 
-	
+    
 
 
 
